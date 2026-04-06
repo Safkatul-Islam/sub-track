0aaa85ee1cbec07e89ab024fdaca3dbbae9da764
Md Safkatul Islam <mdsafkatul.stu24@gmail.com>
2026-04-05 23:51:26 -0700

    initialize project structure with Maven, add application properties, and create entity classes for Subscription and Category

---
+++ src/main/java/com/backendev/subscription_tracker/entity/Subscription.java
@@ -0,0 +1,33 @@
+package com.backendev.subscription_tracker.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "subscriptions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private LocalDate renewalDate;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
