965d02cf546952a8e53b52c21366fe49f1bd2004
Md Safkatul Islam <mdsafkatul.stu24@gmail.com>
2026-04-13 20:14:46 -0700

    add method to find subscriptions by renewal date range

--- src/main/java/com/backendev/subscription_tracker/repository/SubscriptionRepository.java
+++ src/main/java/com/backendev/subscription_tracker/repository/SubscriptionRepository.java
@@ -4,7 +4,13 @@ import com.backendev.subscription_tracker.entity.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+
     boolean existsByName(String name);
+
+    List<Subscription> findByRenewalDateBetween(LocalDate from, LocalDate to);
 }
